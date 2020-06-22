b64919acd73439d3c6b4a344bb18e8bc2637f9f9
Mostafijur Rahman <40955844+MostafijurJ@users.noreply.github.com>
2020-06-22 17:19:30 +0600

    Add files via upload

--- main.go
+++ main.go
@@ -23,7 +23,24 @@ func apiResponse(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+
+func apiRequest(w http.ResponseWriter, r *http.Request) {
+	// Set the return Content-Type as JSON like before
+	w.Header().Set("Content-Type", "application/json")
+
+	// Change the response depending on the method being requested
+	switch r.Method {
+	case "POST":
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(`{"message": "POST method requested"}`))
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte(`{"message": "Can't find method requested"}`))
+	}
+}
+
 func main() {
 	http.HandleFunc("/users",apiResponse)
-	log.Fatal(http.ListenAndServe(":8080",nil))
+	http.HandleFunc("/posts",apiRequest)
+	log.Fatal(http.ListenAndServe(":8000",nil))
 }
