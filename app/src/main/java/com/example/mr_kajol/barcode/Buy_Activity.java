aa1e478f4c7b61c8ea7cf3eca5123d3f7fdfc835
mostafijurj <mrkajol550@gmail.com>
2020-08-06 17:40:58 +0600

    order done

--- app/src/main/java/com/example/mr_kajol/barcode/Buy_Activity.java
+++ app/src/main/java/com/example/mr_kajol/barcode/Buy_Activity.java
@@ -5,13 +5,16 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.DialogFragment;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,7 +26,7 @@ import java.util.Calendar;
 public class Buy_Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, View.OnClickListener {
 
     String[] SizeUnits = { "12 KG", "30 KG", "45 KG"};
-    String[] QtyUnits = { "1", "2", "3","4","5"};
+    String[] QtyUnits = { "1", "2", "3","4"};
 
 
     Calendar calendar = Calendar.getInstance();
@@ -34,25 +37,35 @@ public class Buy_Activity extends AppCompatActivity implements AdapterView.OnIte
     DatePickerDialog.OnDateSetListener setListner;
 
 
-    private CardView CVDateView;
-    private TextView TvDateshow, tvdateclick;
+    private CardView BacktoHome,SubmitBUtton;
+    private TextView TvDateshow, ResultSet;
+    ProgressBar progressBar;
+    Spinner SizeSpin, QtySpin;
+    int sizeindex, qtyindex;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy);
 
-        Spinner SizeSpin = (Spinner) findViewById(R.id.sizeSpinner);
-        Spinner QtySpin = (Spinner) findViewById(R.id.qtySpinner);
+         SizeSpin = (Spinner) findViewById(R.id.sizeSpinner);
+         QtySpin = (Spinner) findViewById(R.id.qtySpinner);
 
-        CVDateView = findViewById(R.id.card_Date);
-        tvdateclick = findViewById(R.id.tvDateclick);
+        BacktoHome = findViewById(R.id.BackCDHome);
+        ResultSet = findViewById(R.id.resultset);
         TvDateshow = findViewById(R.id.tvdateView);
+
+        SubmitBUtton = findViewById(R.id.SubmitButton);
 
       //  CVDateView.setOnClickListener(this);
         //tvdateclick.setOnClickListener(this);
         TvDateshow.setText(" "+day+"/"+month+"/"+year);
+
+
         TvDateshow.setOnClickListener(this);
+        SubmitBUtton.setOnClickListener(this);
+        BacktoHome.setOnClickListener(this);
 
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, SizeUnits);
@@ -66,7 +79,30 @@ public class Buy_Activity extends AppCompatActivity implements AdapterView.OnIte
         QtySpin.setAdapter(qtyadapter);
         QtySpin.setOnItemSelectedListener(this);
 
+        SizeSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                //Toast.makeText(MainActivity.this," Weight", Toast.LENGTH_LONG).show();
 
+                sizeindex = position;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+        });
+        QtySpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                //Toast.makeText(MainActivity.this," Weight", Toast.LENGTH_LONG).show();
+
+                qtyindex = position;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+        });
 
         setListner = new DatePickerDialog.OnDateSetListener() {
             @Override
@@ -115,6 +151,39 @@ public class Buy_Activity extends AppCompatActivity implements AdapterView.OnIte
                 );
 
                 datePickerDialog.show();
+                break;
+            }
+            case R.id.SubmitButton:{
+                String size="", qty="";
+                Toast.makeText(Buy_Activity.this,"Order Submitted", Toast.LENGTH_LONG).show();
+                    String date = TvDateshow.getText().toString();
+                  //Size Disturibution
+                    if(sizeindex ==0)
+                        size = "12 KG";
+                        else if(sizeindex ==1)
+                        size = "30 KG";
+                        else size = "45 KG";
+
+
+                        if(qtyindex ==0)
+                            qty = "1 pics";
+                        else if(qtyindex ==1)
+                            size = "2 pics";
+                        else if(qtyindex == 2 )qty = "3 pics";
+                        else if(qtyindex == 3 )qty = "4 pics";
+
+                        String Details = "Order Details \n"+
+                                        "Size : "+size+
+                                        "\nQuantity: "+qty+
+                                "\nDelevery Date : " +date;
+
+                ResultSet.setText(Details);
+
+                break;
+            }
+            case R.id.BackCDHome:{
+                Intent intent = new Intent(Buy_Activity.this, HomePage.class);
+                startActivity(intent);
                 break;
             }
         }
